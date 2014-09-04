98e4ca2728bfe7930691a5559a12d622a21c5edf
David G Chaves <davidgchaves@gmail.com>
2014-09-04 21:10:40 +0200

    08 - attributes

--- game.rb
+++ game.rb
@@ -1,7 +1,17 @@
 class Player
+  attr_reader :health, :name
+
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
+  end
+
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+
+  def score
+    @health + @name.length
   end
 
   # NOTE: I wonder if the blam and w00t methods
@@ -20,19 +30,24 @@ class Player
   end
 
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 end
 
 
 player1 = Player.new "moe"
 puts player1
+puts player1.health
 
 player2 = Player.new "larry", 60
 puts player2
+puts player2.health
+player2.name = "lawrence"
+puts player2.name
 
 player3 = Player.new "curly", 125
 puts player3
+puts player3.health
 
 player3.blam
 puts player3
