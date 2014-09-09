2b7590c71f10c627e834a1ca64e52cc9ce12bd8f
David G Chaves <davidgchaves@gmail.com>
2014-09-09 11:27:09 +0200

    11 - separate source files

--- game.rb
+++ game.rb
@@ -1,39 +1,3 @@
-class Player
-  attr_reader :health, :name
-
-  def initialize(name, health=100)
-    @name = name.capitalize
-    @health = health
-  end
-
-  def name=(new_name)
-    @name = new_name.capitalize
-  end
-
-  def score
-    @health + @name.length
-  end
-
-  # NOTE: I wonder if the blam and w00t methods
-  #       shouldn't be the perfect candidates for a Writer Monad.
-  #       Obviously there's the issue with the @health mutation,
-  #       ...but the logging part...
-  # NOTE TO SELF: STOP LEARNING HASKELL, I see Monads everywhere!!!!
-  def blam
-    @health -= 10
-    puts "#{@name} got blammed!"
-  end
-
-  def w00t
-    @health += 15
-    puts "#{@name} got w00ted!"
-  end
-
-  def to_s
-    "I'm #{@name} with a health of #{@health} and a score of #{score}."
-  end
-end
-
 class Game
   attr_reader :title
 
@@ -58,13 +22,3 @@ class Game
     end
   end
 end
-
-player1 = Player.new "moe"
-player2 = Player.new "larry", 60
-player3 = Player.new "curly", 125
-
-knuckleheads = Game.new "Knuckleheads"
-knuckleheads.add_player player1
-knuckleheads.add_player player2
-knuckleheads.add_player player3
-knuckleheads.play
---
+++ player.rb
@@ -0,0 +1,35 @@
+class Player
+  attr_reader :health, :name
+
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+
+  def score
+    @health + @name.length
+  end
+
+  # NOTE: I wonder if the blam and w00t methods
+  #       shouldn't be the perfect candidates for a Writer Monad.
+  #       Obviously there's the issue with the @health mutation,
+  #       ...but the logging part...
+  # NOTE TO SELF: STOP LEARNING HASKELL, I see Monads everywhere!!!!
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
+
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
+
+  def to_s
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
+  end
+end
