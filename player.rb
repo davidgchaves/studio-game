dc76dbc51413660478e4723965ae9797d346d465
David G Chaves <davidgchaves@gmail.com>
2014-09-27 09:57:52 +0200

    21 - mixins (playable)

--- player.rb
+++ player.rb
@@ -1,7 +1,11 @@
 require_relative 'treasure_trove'
+require_relative 'playable'
 
 class Player
-  attr_reader :health, :name
+  include Playable
+
+  attr_accessor :health
+  attr_reader :name
 
   def initialize(name, health=100)
     @name = name.capitalize
@@ -15,25 +19,6 @@ class Player
 
   def score
     @health + points
-  end
-
-  def strong?
-    @health > 100
-  end
-
-  # NOTE: I wonder if the blam and w00t methods
-  #       shouldn't be the perfect candidates for a Writer Monad.
-  #       Obviously there's the issue with the @health mutation,
-  #       ...but the logging part...
-  # NOTE TO SELF: STOP LEARNING HASKELL, I see Monads everywhere!!!!
-  def blam
-    @health -= 10
-    puts "#{@name} got blammed!"
-  end
-
-  def w00t
-    @health += 15
-    puts "#{@name} got w00ted!"
   end
 
   def found_treasure(treasure)
